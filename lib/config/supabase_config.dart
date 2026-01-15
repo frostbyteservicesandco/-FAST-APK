07bf75c9c693d6337182b67c2645e0cade215be1
frostbyteservicesandco <frostbyteservicesandco@gmail.com>
2026-01-15 23:23:04 +0530

    Update supabase_config.dart

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,4 +1,5 @@
 class SupabaseConfig {
-  static const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co';
-  static const anonKey = 'YOUR_PUBLIC_ANON_KEY';
+  static const supabaseUrl = 'https://ykljfpfifrmfdykoypex.supabase.co';
+  static const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrbGpmcGZpZnJtZmR5a295cGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MjUxNjksImV4cCI6MjA4NDAwMTE2OX0.eEoBo2MEkgadPmR3kkeg6dNHAAuVuz7xc4DfxPGlsZA';
 }
+
